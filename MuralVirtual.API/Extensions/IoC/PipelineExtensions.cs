98081519ea9a206176076a41befbd9b8568a0867
Flávio Eduardo <xflax00@gmail.com>
2025-03-13 14:26:01 -0300

    feat: Implementing AutoMapper

---
+++ MuralVirtual.API/Extensions/IoC/PipelineExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace MuralVirtual.API.Extensions.IoC;
+
+public static class PipelineExtensions
+{
+    public static void AddApiDI(this IServiceCollection services)
+    {
+        #region Default
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        #endregion Default
+
+        #region AutoMapper
+        services.AddAutoMapper(typeof(Program));
+        #endregion AutoMapper
+    }
+}
--- MuralVirtual.API/Program.cs
+++ MuralVirtual.API/Program.cs
@@ -1,11 +1,10 @@
+using MuralVirtual.API.Extensions.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var services = builder.Services;
+services.AddApiDI();
 
 var app = builder.Build();
 
