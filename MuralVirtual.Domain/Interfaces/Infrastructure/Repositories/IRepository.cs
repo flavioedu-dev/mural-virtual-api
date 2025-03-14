7ae383ca562f96182f71ca5cbc550b7a9bf4cd85
Flávio Eduardo <xflax00@gmail.com>
2025-03-14 13:33:10 -0300

    feat: Implementing Generic Repository patterns with Unit of Work

---
+++ MuralVirtual.Domain/Interfaces/Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace MuralVirtual.Domain.Interfaces.Infrastructure.Repositories;
+
+public interface IRepository<T>
+{
+    Task<List<T>> GetAllAsync();
+
+    Task<T?> GetAsync(Expression<Func<T, bool>> predicate);
+
+    Task<T?> GetByIdAsync(long id);
+
+    Task CreateAsync(T entity);
+
+    Task UpdateAsync(T entity);
+
+    Task DeleteAsync(long id);
+}
---
+++ MuralVirtual.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MuralVirtual.Domain.Interfaces.Infrastructure.Repositories;
+using System.Linq.Expressions;
+
+namespace MuralVirtual.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    protected MuralVirtualDbContext? _context;
+
+    public async Task<T?> GetAsync(Expression<Func<T, bool>> predicate)
+        => await _context!.Set<T>().FindAsync(predicate);
+
+    public async Task<T?> GetByIdAsync(long id)
+        => await _context!.Set<T>().FindAsync(id);
+
+    public async Task<List<T>> GetAllAsync()
+        => await _context!.Set<T>().ToListAsync();
+
+    public async Task CreateAsync(T entity)
+        => await _context!.Set<T>().AddAsync(entity);
+
+    public Task UpdateAsync(T entity)
+    {
+        _context!.Set<T>().Update(entity);
+
+        return Task.CompletedTask;
+    }
+
+    public async Task DeleteAsync(long id)
+    {
+        T? entity = await GetByIdAsync(id);
+
+        if (entity is null)
+            return;
+
+        _context!.Set<T>().Remove(entity);
+    }
+}
